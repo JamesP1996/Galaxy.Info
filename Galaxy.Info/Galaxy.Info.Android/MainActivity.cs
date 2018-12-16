a9bc28e072fe52b33bcafba71ddd2d6233443c11
JamesP1996 <43405677+JamesP1996@users.noreply.github.com>
2018-12-16 22:35:13 +0000

    Added a Icon for Windows+Android
    Added icons for Android (drawablefolder) Icon and Windows.
    Also Extra Updates

--- Galaxy.Info/Galaxy.Info.Android/MainActivity.cs
+++ Galaxy.Info/Galaxy.Info.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Galaxy.Info.Droid
 {
-    [Activity(Label = "Galaxy.Info", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Galaxy.Info", Icon = "@drawable/app_icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
